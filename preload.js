06a8724c7a220f1891a98f7956a8faba016e988b
aetherlaguna <alan1999gustavo99@gmail.com>
2025-07-29 22:19:03 -0300

    chore: removing other files

--- preload.js
+++
@@ -1,5 +0,0 @@
-const {contextBridge, ipcRenderer} = require('electron')
-
-contextBridge.exposeInMainWorld('api', {
-  test: () => ipcRenderer.invoke('test')
-})
--- renderer.js
+++
@@ -1,6 +0,0 @@
-async function test(){
-  const response = await window.api.test()
-  console.log(response);
-}
-
-test()
